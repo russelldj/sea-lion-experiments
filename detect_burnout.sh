188958fe261400eb7ce74c314ddb4beb3e435bf5
David Russell <david.russell@kitware.com>
2021-03-14 19:56:44 -0400

    Update pipelines

--- detect_burnout.sh
+++ detect_burnout.sh
@@ -1,3 +1,3 @@
 #gdb -ex "set breakpoint pending on" \
-#	-ex run --args kwiver 
-runner detector_burnout.pipe -s input:video_filename=input_images.txt
+#	-ex run --args
+kwiver runner detector_burnout.pipe -s input:video_filename=input_images.txt
