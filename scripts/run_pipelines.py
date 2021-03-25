20ec12917f4e8766e3bf0d9ba6ecf110d50b9e76
David Russell <david.russell@kitware.com>
2021-03-24 21:53:56 -0400

    Working runner script

--- scripts/run_pipelines.py
+++ scripts/run_pipelines.py
@@ -4,6 +4,7 @@ import pathlib
 import pandas as pd
 import argparse
 from glob import glob
+import ubelt as ub
 
 import pdb
 
@@ -20,12 +21,28 @@ os.chdir(PIPELINE_DIR)
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--method", default=METHOD)
+    parser.add_argument("--debug", action="store_true")
     args = parser.parse_args()
     return args
 
-def compute(root_dir=ROOT_DIR, method=METHOD, output_dir=OUTPUT_DIR, image_list=IMAGE_LIST, extension=EXTENSTION ):
+def fix_frame_number(input_filename):
+    print(input_filename)
+    os.system(f"dvc unprotect {input_filename}")
+    data = pd.read_csv(input_filename, names=range(16))
+    filenames = data.iloc[:, 1].copy()
+    unique_filenames = filenames.unique()
+    unique_filenames = unique_filenames.tolist()
+    unique_filenames = pd.Index(unique_filenames)
+    filename_indices = [unique_filenames.get_loc(filename) for filename in filenames.tolist()]
+    print(data)
+    data.iloc[:, 2] = filename_indices
+    print(data)
+    data.to_csv(input_filename, header=False, index=False)
+
+def compute(root_dir=ROOT_DIR, method=METHOD, output_dir=OUTPUT_DIR, image_list=IMAGE_LIST, extension=EXTENSTION,
+            debug=False ):
     folders = list(sorted(filter(os.path.isdir, glob(os.path.join(root_dir, "*")))))
-    for folder in folders[:1]:
+    for folder in folders:
         annotation_file = os.path.join(folder, "sealions_" + os.path.basename(folder) + "*.viame.csv")
         annotation_file = glob(annotation_file)
         if len(annotation_file) != 1:
@@ -33,20 +50,24 @@ def compute(root_dir=ROOT_DIR, method=METHOD, output_dir=OUTPUT_DIR, image_list=
         annotation_file=annotation_file[0]
         if not os.path.isfile(annotation_file):
             continue
-
+        fix_frame_number(annotation_file)
         image_names = pd.read_csv(annotation_file, squeeze=True, usecols=[1]).unique()
+        # TODO update this
         image_files = list(map( lambda x : os.path.join(folder, x), image_names))
 
         with open(image_list, "w") as fout:
-            fout.write("\n".join(image_files[:1]))
-        func = os.system
+            fout.write("\n".join(image_files))
 
-        func(f"gdb --args kwiver runner {method}.pipe " +
+        cmd_string = (f"kwiver runner {method}.pipe " +
              f" --setting input:video_filename='{image_list}' --setting " +
              f"detector_writer:file_name='{os.path.join(output_dir, method + '_' + os.path.basename(folder))}.csv' "
              f"--setting detection_reader:file_name='{annotation_file}'")
 
+        if debug:
+            cmd_string = "gdb -ex 'run' --args " + cmd_string
+        ub.cmd(cmd_string, verbose=3)
+
 args = parse_args()
 
-compute(method=args.method)
+compute(method=args.method, debug=args.debug)
 #compute(method="utility_add_segmentations_grabcut")
